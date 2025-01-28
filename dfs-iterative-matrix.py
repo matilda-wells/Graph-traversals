3cc6793c2cff338db445673b8043084c752940dd
Matilda Wells <159696579+matilda-wells@users.noreply.github.com>
2025-01-28 12:31:44 +0000

    commit

--- dfs-iterative-matrix.py
+++ dfs-iterative-matrix.py
@@ -7,16 +7,33 @@ graph = [
 	[0,0,1,0,1,0]
 ]
 
+VISITED = 0
+UNVISITED = 0
 
 def dfs(graph, start):
     visited = [False] * len(graph)
     stack = [start]
-    visited[start] = True
+    # visited[start] = True
 
+    # while len(stack) > 0:
+    #     for i in range(len(visited)):
+    #         if  graph[start][i] == 1 and visited[i] == False:
+    #             dfs(graph, i)
+
+    visited[start] = True
+    current = start
     while len(stack) > 0:
-        for i in range(len(visited)):
-            if  graph[start][i] == 1 and visited[i] == False:
-                dfs(graph, i)
+        found = False
+        # find an unvisited neighbour of the current node
+        for neighbour in range(len(graph)):
+            if graph[current][neighbour] == 1 and visited[neighbour] == False:
+                stack.append(neighbour)
+                found = True
+        # no unvisited neighbours - pop from the stack
+        if not found:
+            current = stack.pop()
+
+
 
 dfs(graph, 0)
 
